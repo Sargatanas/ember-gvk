2d88be15a337e311079868f38c45ff04f26ffb11
Permyakova Ksenya <kpermyakova@ics.perm.ru>
2019-11-22 15:10:54 +0500

    Добавлено: простейшее отображение заявок на /tasks

--- app/models/task.js
+++ app/models/task.js
@@ -1,17 +1,41 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  index: DS.attr(),
-  importance: DS.attr(),
-  adress: {
-      street: DS.attr(),
-      house: DS.attr(),
-      building: DS.attr(),
-      flat: DS.attr()
-  },
+  index: DS.attr('number'),
+  importance: DS.attr('number'),
+
+  adress: DS.attr('object', {
+    street: DS.attr('string'),
+    house: DS.attr('number'),
+    building: DS.attr('string'),
+    flat: DS.attr('number'),
+  }),
+  
   targets: DS.attr(),
-  planDuration: {
-    hours: DS.attr(),
-    minutes: DS.attr()
-  }
+
+  planeDuration: DS.attr('object', {
+    hours: DS.attr('number'),
+    minutes: DS.attr('number'),
+  }),
+
+  fullAdress: Ember.computed('adress', function() {
+    let adress = 'Ул. ';
+    adress += this.get('adress').street + ', д.';
+    adress += this.get('adress').house;
+
+    let building = this.get('adress').building;
+    adress += building ? ', корп.' + building : '';
+
+    let flat = this.get('adress').flat;
+    adress += flat ? ', кв.' + flat : '';
+
+    return adress;
+  }),
+
+  planeTime: Ember.computed('planeDuration', function() {
+    let minutes = this.get('planeDuration').minutes;
+    minutes = minutes < 10 ? '0' + minutes: minutes;
+
+    return `${this.get('planeDuration').hours}:${minutes}`;
+  }),
 });
--- app/routes/task.js
+++ app/routes/task.js
@@ -2,9 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        /* return this.store.findAll('task'); */
-
-        return fetch('http://localhost:3000/tasks')
-          .then((response) => response.json());
+        return this.store.findAll('task');
     }
 });
