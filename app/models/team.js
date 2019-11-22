c228f8799cd688907d216812e2786d03e7085f49
Permyakova Ksenya <kpermyakova@ics.perm.ru>
2019-11-22 16:11:54 +0500

    Добавлено: модель бригады с выводом списка бригад под списком заявок

---
+++ app/models/team.js
@@ -0,0 +1,27 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  index: DS.attr('number'),
+  shiftStart: DS.attr('object', {
+      hours: DS.attr('number'),
+      minutes: DS.attr('number'),
+  }),
+  shiftEnd: DS.attr('object', {
+    hours: DS.attr('number'),
+    minutes: DS.attr('number'),
+  }),
+
+  timeStart: Ember.computed('shiftStart', function() {
+    let minutes = this.get('shiftStart').minutes;
+    minutes = minutes < 10 ? '0' + minutes: minutes;
+
+    return `${this.get('shiftStart').hours}:${minutes}`;
+  }),
+
+  timeEnd: Ember.computed('shiftEnd', function() {
+    let minutes = this.get('shiftEnd').minutes;
+    minutes = minutes < 10 ? '0' + minutes: minutes;
+
+    return `${this.get('shiftEnd').hours}:${minutes}`;
+  }),
+});
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model() {
+    /* model() {
         return this.store.findAll('task');
-    }
+    } */
+
+    model() {
+        return Ember.RSVP.hash({
+          tasks: this.store.findAll('task'),
+          teams: this.store.findAll('team')
+        });
+      },
+    
+      setupController(controller, model) {
+        this._super(...arguments);
+        Ember.set(controller, 'tasks', model.tasks);
+        Ember.set(controller, 'teams', model.teams);
+      }
 });
