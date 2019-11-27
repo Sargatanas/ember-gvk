2819ee9656565d5db8034c025a19c4ebc49d5b6d
Permyakova Ksenya <kpermyakova@ics.perm.ru>
2019-11-27 14:24:52 +0500

    Добавлено: возможность свернуть-развернуть заявки

--- app/components/form-task.js
+++ app/components/form-task.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    isOpen: false,
+    class: 'element-body-task_resize',
+
+    actions: {
+        toggleTask() {
+            if (this.get('class') === 'element-body-task_resize') {
+                this.setProperties({
+                    isOpen: true,
+                    class: 'element-body-task_open'
+                });
+            } else {
+                this.setProperties({
+                    isOpen: false,
+                    class: 'element-body-task_resize'
+                });
+            }
+        }
+    }
 });
