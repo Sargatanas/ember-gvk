6b32a3c57c22ab27f8d5adb2a1b16c45a6a0a10c
Permyakova Ksenya <kpermyakova@ics.perm.ru>
2019-11-29 10:35:51 +0500

    Промежуточное: утилита для определения тех заявок, которые могут быть выполнены еще в текущий день #2

--- app/utils/task-can-be-done.js
+++ app/utils/task-can-be-done.js
@@ -1,15 +1,85 @@
+import dateNullable from '../utils/date-nullable';
+import dateForm from '../utils/date-form';
+
 export default function taskCanBeDone(teams, tasks) {
   let team = teams[0];
 
   let teamTasks = team.get('tasks');
-  let latestTask = '';
-  teamTasks.forEach(teamTask => {
-    latestTask = latestTask === '' ? teamTask : latestTask;
-    
-    let latestTime = latestTask.get('timeStart').hours*60 + latestTask.get('timeStart').minutes;
-    let currentTime = teamTask.get('timeStart').hours*60 + teamTask.get('timeStart').minutes;
+  let lastTask = getLastTask(teamTasks);
+  
+  let date = lastTask === '' ? new Date() : new Date(lastTask.get('date'));
+  date = dateNullable(date);
 
-    latestTask = currentTime > latestTime ? teamTasks : latestTask;
+  let shiftEnd = new Date(date);
+  shiftEnd.setHours(team.get('shiftEnd').hours);
+
+  lastTask = getTaskInfo(lastTask, date);
+  console.log('Последняя запланировнная заявка');
+  console.log(lastTask);
+  console.log('');
+
+  let tasksPlane = [];
+  tasks.forEach((task, index) => {
+    console.log(`Заявка ${task.get('index')}`); 
+
+    let taskStart = new Date(dateForm(lastTask.end));
+    taskStart.setHours(lastTask.end.getHours() + 1);
+    taskStart.setMinutes(lastTask.end.getMinutes());
+
+    let taskInfo = getTaskInfo(task, date, taskStart);
+
+    console.log(taskInfo);    
+    console.log(`Окончание заявки ${index}. ${task.get('index')}`);
+    console.log(taskInfo.end);
+    
+    if (taskInfo.end.getTime() <= shiftEnd.getTime()) {
+      console.log(`Заявка подходит`);
+      lastTask = taskInfo;
+
+      tasksPlane.push(task);
+    }  else {
+      console.log(`Заявка не подходит`);
+    }
+    console.log(tasksPlane);
+    console.log('');
   });
-  console.log(latestTask);
 }
+
+function getLastTask(tasks) {
+  let lastTask = '';
+
+  tasks.forEach(task => {
+    lastTask = lastTask === '' ? task : lastTask;
+    
+    let lastTime = lastTask.get('timeStart').hours*60 + lastTask.get('timeStart').minutes;
+    let currentTime = task.get('timeStart').hours*60 + task.get('timeStart').minutes;
+
+    lastTask = currentTime > lastTime ? task : lastTask;
+  });  
+  
+  return lastTask;
+}
+
+function getTaskInfo(task, date, start) {
+  let taskDuration = task.get('planeDuration');
+
+  let taskStart = '';
+    if (start) {
+    taskStart = start;
+  } else {
+    taskStart = new Date(date);
+    taskStart.setHours(task.get('timeStart').hours);
+    taskStart.setMinutes(task.get('timeStart').minutes);
+  }  
+
+  let taskEnd = new Date(date);
+  taskEnd.setHours(taskStart.getHours()+ taskDuration.hours);
+  taskEnd.setMinutes(taskStart.getMinutes() + taskDuration.minutes);
+
+  return {
+    task: task,
+    start: taskStart,
+    end: taskEnd
+  }
+}
+
