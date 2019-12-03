e6b2dc7efcc8b1b3e75d27d685d527d631c649e5
Permyakova Ksenya <kpermyakova@ics.perm.ru>
2019-12-03 14:29:38 +0500

    Добавлено: Практическое задание №5 (с возможностью существования неработающих бригад)

--- public/resources/db/ember_gvk_select_5.sql
+++ public/resources/db/ember_gvk_select_5.sql
@@ -1,8 +1,19 @@
-SELECT 
+SELECT
+teams.index AS "Бригада",
+'00:00:00' AS "Общяя длительность работ"
+FROM public.requests 
+RIGHT JOIN public.teams ON requests.team=teams.id
+WHERE requests.team IS NULL
+
+UNION
+
+SELECT
 teams.index AS "Бригада",
 SUM(requests.real_duration) AS "Общяя длительность работ"
 FROM public.requests 
 JOIN public.teams ON requests.team=teams.id
 GROUP BY teams.index
-ORDER BY SUM(requests.real_duration)
+
+ORDER BY "Общяя длительность работ"
+
 LIMIT 5;
